739ce328c4f764dc315f0dd9bf419a501bc42203
James Halliday <mail@substack.net>
2011-10-03 07:37:59 -0700

    include and exclude core tests

--- test/core.js
+++ test/core.js
@@ -1,16 +1,23 @@
 var test = require('tap').test;
 var subdeps = require('../');
 
-test('include core', function (t) {
-    var deps = subdeps.sync('/a.js', {
+test('core', function (t) {
+    var depsInc = subdeps.sync('/a.js', {
         sources : {
             '/a.js' : "var b = require('./b');",
             '/b.js' : "var EventEmitter = require('events').EventEmitter"
         },
         core : true
     });
+    t.deepEqual(depsInc, [ '/a.js', '/b.js', 'events' ]);
     
-    t.deepEqual(deps, [ '/a.js', '/b.js', 'events' ]);
+    var depsEx = subdeps.sync('/a.js', {
+        sources : {
+            '/a.js' : "var b = require('./b');",
+            '/b.js' : "var EventEmitter = require('events').EventEmitter"
+        }
+    });
+    t.deepEqual(depsEx, [ '/a.js', '/b.js' ]);
     
     t.end();
 });
